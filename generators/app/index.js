656a7afce4f205c94bab4810dda639de8ffe146c
Federico Bartolomei <fbarto01@mail.bbk.ac.uk>
2018-06-03 19:43:33 +0100

    Added ring/compojure/heroku/reagent support

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,4 +1,6 @@
 'use strict';
+
+const chalk = require('chalk');
 const slugify = require("underscore.string/slugify");
 const underscored = require("underscore.string/underscored");
 
@@ -10,12 +12,19 @@ module.exports = class extends Generator {
   }
 
   _setProject() {
-    return {
-      type: 'input',
-      name: 'project-name',
-      message: 'Enter your project name',
-      default: 'hello-seymore'
-    }
+    return [{
+        type: 'input',
+        name: 'project-name',
+        message: 'Enter your project name',
+        default: 'hello-seymore'
+      },
+      {
+        type: 'input',
+        name: 'project-version',
+        message: 'Enter the project version',
+        default: '0.1.0-SNAPSHOT'
+      }
+    ]
   }
 
   _copy(appName, from, to, opts) {
@@ -37,18 +46,31 @@ module.exports = class extends Generator {
   }
 
   go() {
-    this.prompt([this._setProject()]).then((answers) => {
+    this.prompt(this._setProject()).then((answers) => {
       const app_name = answers['project-name'];
+      const app_version = answers['project-version'];
       const opts = {
         app_name: app_name,
+        version: app_version,
         app_path: `${slugify(app_name)}`
       };
       const [clj, cljs] = this.srcTemplatePaths(app_name);
-      this.log(answers);
       this._write('project.clj', `${app_name}/project.clj`, opts);
       this._write('assets', app_name, opts);
       this._write('src/clj', clj, opts);
       this._write('src/cljs', cljs, opts);
+
+      this.log(chalk.bold.green(`${app_name} created succesfully.`));
+      this.log('To run the server, execute ' +
+        chalk.yellow('lein ring server'));
+      this.log('To run the frontend, execute ' +
+        chalk.yellow('lein figwheel'));
+      this.log('To run server and frontend on the same shell, execute ' +
+        chalk.yellow('lein cooper'));
+      this.log('To build an uberjar, execute ' +
+        chalk.yellow('lein package'));
+      this.log('To deploy to heroku, execute ' +
+        chalk.yellow(`heroku create ${app_name} && lein heroku deploy`));
     });
   }
 };
--- generators/app/templates/project.clj
+++ generators/app/templates/project.clj
@@ -1,4 +1,4 @@
-(defproject <%= app_name %> "0.1.0-SNAPSHOT"
+(defproject <%=app_name%> "<%=version%>"
             :min-lein-version "2.0.0"
             :source-paths ["src/clj"]
             :dependencies [[org.clojure/clojure "1.8.0"]
@@ -12,18 +12,36 @@
                            [reagent "0.8.1"]]
             :plugins [[lein-ring "0.9.7"]
                       [lein-figwheel "0.5.13"]
-                      [lein-cooper "1.2.2"]]
+                      [lein-cooper "1.2.2"]
+                      [lein-cljsbuild "1.1.5"
+                       :exclusions [[org.clojure/clojure]]]
+                      [lein-heroku "0.5.3"]]
             :cooper {"fig" ["lein" "figwheel"]
                      "ring"  ["lein" "ring" "server"]}
             :ring {:handler <%= app_path %>.handler/app}
             :clean-targets [:target-path "out"]
-            :cljsbuild {:builds [{:id "<%= app_name %>"
+            :cljsbuild {:builds [{:id "dev"
                                   :source-paths ["src/cljs"]
                                   :figwheel true
-                                  :compiler {:main "<%= app_path %>.app"
+                                  :compiler {:main "<%=app_path%>.app"
                                              :asset-path "js/out"
                                              :output-to "resources/public/js/main.js"
-                                             :output-dir "resources/public/js/out"}}]}
+                                             :output-dir "resources/public/js/out"}}
+                                 {:id "min"
+                                  :source-paths ["src/cljs"]
+                                  :compiler {:main "<%=app_path%>.app"
+                                             :output-to "resources/public/js/main.js"
+                                             :optimizations :advanced
+                                             :pretty-print false}}]}
             :figwheel {:css-dirs ["resources/public/css"]}
+            :aliases {"package" ["do" "clean"
+                                 ["cljsbuild" "once" "min"]
+                                 ["ring" "uberjar"]]}
+            :heroku {:app-name "<%= app_name %>"
+                     :jdk-version "1.8"
+                     :include-files ["target/<%= app_name %>-<%= version %>-standalone.jar"]
+                     :process-types {"web" "java -jar target/<%=app_name%>-<%=version%>-standalone.jar"}}
+
+            :uberjar-name "<%=app_name%>-<%=version%>-standalone.jar"
             :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                                             [ring/ring-mock "0.3.0"]]}})
